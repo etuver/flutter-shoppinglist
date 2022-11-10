5e4c324a810f90dd451139d424303869126bcbcc
Eventu <even.tuverud@gmail.com>
2022-11-10 12:53:45 +0100

    Add Shoppinglist and make ontap change checked status

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Listing.dart';
+import 'ShoppingList.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,25 +15,44 @@ void main() {
   //items.add("Vaskemiddel");
   //items.add("Smør");
   //items.add("Zalo");
-  shoppingList.add(Listing(name: "Potet"));
-  shoppingList.add(Listing(name: "Gulrot"));
-  shoppingList.add(Listing(name: "Kjøtt"));
-  shoppingList.add(Listing(name: "Epler"));
-  shoppingList.add(Listing(name: "Buljong"));
-  shoppingList.add(Listing(name: "Egg"));
-  shoppingList.add(Listing(name: "Makaroni"));
-  shoppingList.add(Listing(name: "Brød"));
-  shoppingList.add(Listing(name: "Vaskemiddel"));
-  shoppingList.add(Listing(name: "Dopapir"));
-  shoppingList.add(Listing(name: "Smør"));
-  shoppingList.add(Listing(name: "Zalo"));
-  shoppingList.add(Listing(name: "Løk"));
-  shoppingList[5].checked = true;
+  //shoppingList.add(Listing(name: "Potet"));
+  //shoppingList.add(Listing(name: "Gulrot"));
+  //shoppingList.add(Listing(name: "Kjøtt"));
+  //shoppingList.add(Listing(name: "Epler"));
+  //shoppingList.add(Listing(name: "Buljong"));
+  //shoppingList.add(Listing(name: "Egg"));
+  //shoppingList.add(Listing(name: "Makaroni"));
+  //shoppingList.add(Listing(name: "Brød"));
+  //shoppingList.add(Listing(name: "Vaskemiddel"));
+  //shoppingList.add(Listing(name: "Dopapir"));
+  //shoppingList.add(Listing(name: "Smør"));
+  //shoppingList.add(Listing(name: "Zalo"));
+  //shoppingList.add(Listing(name: "Løk"));
+  //shoppingList[5].checked = true;
+  shoppingList.addListing(Listing(name: "Potet"));
+  shoppingList.addListing(Listing(name: "Gulrot"));
+  shoppingList.addListing(Listing(name: "Kjøtt"));
+  shoppingList.addListing(Listing(name: "Epler"));
+  shoppingList.addListing(Listing(name: "Buljong"));
+  shoppingList.addListing(Listing(name: "Egg"));
+  shoppingList.addListing(Listing(name: "Makaroni"));
+  shoppingList.addListing(Listing(name: "Brød"));
+  shoppingList.addListing(Listing(name: "Vaskemiddel"));
+  shoppingList.addListing(Listing(name: "Dopapir"));
+  shoppingList.addListing(Listing(name: "Smør"));
+  shoppingList.addListing(Listing(name: "Zalo"));
+  shoppingList.addListing(Listing(name: "Løk"));
+  shoppingList.changeCheckedStatus("Løk");
+  shoppingList.changeCheckedStatus("Vaskemiddel");
+  shoppingList.changeCheckedStatus("Egg");
+  shoppingList.changeCheckedStatus("Buljong");
+  shoppingList.sortListByChecked();
 }
 
 TextEditingController newItemController = TextEditingController();
-List<String> items = ["Dopapir"];
-List<Listing> shoppingList = [];
+//List<String> items = ["Dopapir"];
+//List<Listing> shoppingList = [];
+ShoppingList shoppingList = ShoppingList(listName: "Handleliste");
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -57,13 +77,19 @@ class _MyAppState extends State<MyApp> {
                 Row(children: [
                   Expanded(
                       child: TextField(
+                    decoration: const InputDecoration(
+                      hintText: "Varenavn",
+                      border: OutlineInputBorder(),
+                    ),
                     controller: newItemController,
                   )),
                   ElevatedButton(
                       onPressed: () {
-                        setState(() {
-                          addToList();
-                        });
+                        if (newItemController.text.isNotEmpty) {
+                          setState(() {
+                            addToList();
+                          });
+                        }
                       },
                       child: const Text("Legg til"))
                 ]),
@@ -78,16 +104,29 @@ class _MyAppState extends State<MyApp> {
                         // Inner listview must be not scrollable
                         shrinkWrap: true,
                         //itemCount: items.length,
-                        itemCount: shoppingList.length,
+                        //itemCount: shoppingList.length,
+                        itemCount: shoppingList.getListingsLength(),
                         itemBuilder: (BuildContext context, int index) {
                           return Card(
                             child: ListTile(
-                                // title: Text(items[index]),
-                                title: Text(shoppingList[index].name),
-                                //trailing: Icon(Icons.done),
-                                trailing: Icon(shoppingList[index].checked
-                                    ? Icons.done
-                                    : Icons.file_download_done)),
+                              onTap: () {
+                                setState(() {
+                                  shoppingList.changeCheckedStatus(
+                                      shoppingList.listings[index].name);
+                                });
+                              },
+                              // title: Text(items[index]),
+                              title: Text(shoppingList.listings[index].name),
+                              //trailing: Icon(Icons.done),
+                              trailing: Icon(
+                                  shoppingList.listings[index].checked
+                                      ? Icons.done
+                                      : Icons.file_download_done),
+                              tileColor: shoppingList.listings[index].checked
+                                  ? Color(0xFF9E9E9E) //Color(0xFF84FFFF)
+                                  : null,
+                              //: Color(0xFF9E9E9E),
+                            ),
                           );
                         },
                       )
@@ -107,7 +146,8 @@ final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 /// Sends a snackbar to the user
 /// Clears the TextField
 void addToList() {
-  shoppingList.add(Listing(name: newItemController.text.toString()));
+  //shoppingList.add(Listing(name: newItemController.text.toString()));
+  shoppingList.addListing(Listing(name: newItemController.text.toString()));
   sendSnackBar("${newItemController.text} lagt til!", Colors.green);
   newItemController.text = "";
 }
