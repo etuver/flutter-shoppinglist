3262632bd1b42fc358b854696b7e0f0f8eb468ee
Eventu <even.tuverud@gmail.com>
2022-11-12 20:22:43 +0100

    Create a Listing class

--- lib/Listing.dart
+++ lib/Listing.dart
@@ -1,3 +1,6 @@
+/// Simple class representing a listing for a Shoppinglist
+/// used for ShoppingList
+/// Can be either an item to buy or a task todo if used as todo- app
 class Listing {
   String name;
   bool checked = false;
