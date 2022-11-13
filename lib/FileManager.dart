6508d537783acdfdc4d2215e7435f5acef33b7d2
Eventu <even.tuverud@gmail.com>
2022-11-14 00:25:48 +0100

    Updated method to read from files in app documents instead of assets, and can now read to files properly

--- lib/FileManager.dart
+++ lib/FileManager.dart
@@ -3,10 +3,83 @@ import 'dart:io';
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:convert';
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shoppinglist/ShoppingList.dart';
 import 'package:shoppinglist/main.dart';
 
 import 'Listing.dart';
+
+/// REads a single json-file from given path and returns as a ShoppingList object
+Future<ShoppingList> getShoppingListFromJsonFile(String filepath) async {
+  final file = File(filepath);
+  final json = await file.readAsString();
+  var data = jsonDecode(json);
+  //String listname = data['listname'];
+  //print("json: " + data);
+  ShoppingList shoppingList = ShoppingList.fromJson(jsonDecode(json));
+  print("shoppinglist: " + shoppingList.listName);
+  return shoppingList;
+}
+
+/// Reads all json files in the app documents directory and
+/// creates a list of shoppinglists for each of the files
+/// should be run on app initialization
+/// directory path depends on device type (ios, android...)
+Future<List<ShoppingList>> readJsonFiles() async {
+  List<ShoppingList> list = [];
+  List<String> files = [];
+  final directory = await getApplicationDocumentsDirectory();
+  List<FileSystemEntity> entities = await directory
+      .list()
+      .where((event) => event.path.endsWith('.json'))
+      .toList();
+  //final Iterable<File> files = entities.whereType<File>();
+
+  for (int i = 0; i < entities.length; i++) {
+    files.add(entities[i].path);
+  }
+  for (int i = 0; i < files.length; i++) {
+    Future<ShoppingList> future = getShoppingListFromJsonFile(files[i]);
+    future.then((value) => list.add(value));
+  }
+  //files.forEach(print);
+  return list;
+}
+
+/// write a shoppingList as a json file
+/// saved in appdata directory
+/// path depending on device type
+void writeListAsJSONFile(ShoppingList shoppingList) async {
+  try {
+    final directory = await getApplicationDocumentsDirectory();
+    String filename = "${shoppingList.listName}.json";
+    File file = File("${directory.path}/$filename");
+    String jsonShoppingList = jsonEncode(shoppingList);
+    file.writeAsString(jsonShoppingList);
+    print("List write to file");
+  } catch (e) {
+    print(e);
+  }
+}
+
+/// Deletes a json file found by listname
+///for when a list is deleted by user
+void deleteJsonfile(String listName) async {
+  final directory = await getApplicationDocumentsDirectory();
+  String filename = "$listName.json";
+  File file = File("${directory.path}/$filename");
+  try {
+    file.delete();
+  } catch (e) {
+    print(e);
+  }
+}
+
+/// encode a shoppinglist to Json object
+String convertToJSON(ShoppingList shoppingList) {
+  String jsonShoppingList = jsonEncode(shoppingList);
+  return jsonShoppingList;
+}
 
 /// Creates a list of all jsonfiles in directory assets/jsonlists
 /// Then calls method getJsonDataByFilePath to get each file read as a ShoppingList
@@ -52,6 +125,8 @@ Future<ShoppingList> getJsonDataByFilePath(String filepath) async {
   }
   return shoppingList;
 }
+
+/////////////////////////////////////////////// OLD ////////////////////////////////////
 
 ///reads a single json file and returnds as future shoppinglist
 ///Not in use
